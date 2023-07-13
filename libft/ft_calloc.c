9b97993ace205c4264e70e936bab77d42c456a06
Camille Ko <camilleko@Camilles-MacBook-Pro.local>
2023-07-13 22:25:47 +0930

    added ft_strdup.c

---
+++ libft/ft_calloc.c
@@ -0,0 +1,15 @@
+//#include "./libft.h"
+#include <unistd.h>
+#include <stdio.h>
+
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
